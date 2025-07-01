3f0fd10a28fc8a8b6983acc34ef1ac8edbe0d6b1
Will Harrison <willharrison2016@gmail.com>
2025-07-01 12:37:23 -0500

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        electric: '#00FFC2',
+        midnight: '#161616',
+        pinknoise: '#F81894',
+      },
+    },
+  },
+  plugins: [],
+}
